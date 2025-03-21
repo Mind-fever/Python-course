cbdeaa09454250a5d31d7305f2bf9159d3716c53
Santiago Martinez <82995832+Mind-fever@users.noreply.github.com>
2025-03-21 18:54:48 -0300

    variables

--- tmcdata/mooc-data-analysis-with-python-2024-2025/part01-e04_multiplication_table/src/multiplication_table.py
+++ tmcdata/mooc-data-analysis-with-python-2024-2025/part01-e04_multiplication_table/src/multiplication_table.py
@@ -2,7 +2,9 @@
 
 
 def main():
-    pass
-
+    for i in range(1,11):
+        for x in range(1,11):
+            print("{:4}".format(i*x),end="")
+        print()
 if __name__ == "__main__":
     main()
--- tmcdata/mooc-data-analysis-with-python-2024-2025/part01-e05_two_dice/src/two_dice.py
+++ tmcdata/mooc-data-analysis-with-python-2024-2025/part01-e05_two_dice/src/two_dice.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
-
+    for i in range(1,7):
+        for x in range(1,7):
+            if i+x==5:
+                print(f"({i},{x})")
 if __name__ == "__main__":
     main()
